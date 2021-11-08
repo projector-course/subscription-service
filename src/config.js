caac22e0552453e372a5df49b62aeef34084ad61
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2021-11-08 18:27:47 +0200

    feat: create service structure + route health

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const HOST = '127.0.0.1';
+const PORT = 3002;
+const BASE_URL = `http://${HOST}:${PORT}`;
+const SERVICE_NAME = 'SUBSCRIPTION SERVICE';
+
+module.exports = {
+  PORT,
+  BASE_URL,
+  SERVICE_NAME,
+};
